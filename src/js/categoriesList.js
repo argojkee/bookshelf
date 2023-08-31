8625c2f5be4903917def96e673c7333a161408c4
Ursx2204 <127949449+Yurii2276@users.noreply.github.com>
2023-08-31 20:08:34 +0300

    категории вивод без стилізації

--- src/js/categoriesList.js
+++ src/js/categoriesList.js
@@ -1,1 +1,34 @@
-console.log(`privet`)
+import { fetchCategoriesList } from '../services/books-api';
+
+const refs = {
+    container: document.querySelector(`.categories-container`),
+};
+
+console.log(`privet`);
+
+fetchCategoriesList()
+  .then(response => {
+      const categories = response.data;
+      const markup = createMarkup(categories);    
+      refs.container.insertAdjacentHTML(`afterbegin`,
+          `<h2 class="title-categoris">All categories</h2>${markup}`);  
+      
+      console.log('array0', categories[0]);
+      console.log('array1', categories[1]);
+      console.log('Categories list:', categories);
+      
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+
+  
+
+function createMarkup(array) { 
+    return array.map(categori => 
+        `<ul>
+            <li class="list-category">${categori.list_name}</li>
+        </ul> 
+        `
+        ).join(``);
+}
