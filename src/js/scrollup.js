21d8e5674198582025c6dcd35d9abc8de0a5adbd
DmShko <vombat_schev@i.ua>
2023-09-02 00:05:11 +0300

    add-scrollUP-modify
    Modify scrollUp button according to the layout.

--- src/js/scrollup.js
+++ src/js/scrollup.js
@@ -2,28 +2,29 @@ import '../css/scrollUP.css'
 
 //get button element
 const elementsSet = {
-    upButton: document.querySelector('.button-up'),
+    case: document.querySelector('.buttonup-case'),
+    upButton: document.querySelector('.buttonup'),
 };
 
 let upButVieW = (evt) => {
  
     switch(evt.type) {
         case 'scroll':
-            if(Math.floor(window.scrollY)> window.innerHeight / 2) { 
-                elementsSet.upButton.style.visibility = "visible";
+            if(Math.floor(window.scrollY) >= window.innerHeight / 2) { 
+                elementsSet.case.style.visibility = "visible";
                 elementsSet.upButton.classList.add("anima-up");
             }
             else {
-                elementsSet.upButton.style.visibility = "hidden";
+                elementsSet.case.style.visibility = "hidden";
                 elementsSet.upButton.classList.remove("anima-up");
             }
             break; 
         case 'mouseover':
-            elementsSet.upButton.style.outline = "2px solid grey";
+            elementsSet.case.style.outline = "2px solid grey";
         
             break; 
         case 'mouseout':
-            elementsSet.upButton.style.outline  = "none";
+            elementsSet.case.style.outline  = "none";
          
             break;
         case 'click':
@@ -37,6 +38,6 @@ let upButVieW = (evt) => {
 };
 
 document.addEventListener('scroll', upButVieW);
-elementsSet.upButton.addEventListener('mouseover', upButVieW);
-elementsSet.upButton.addEventListener('mouseout', upButVieW);
+elementsSet.case.addEventListener('mouseover', upButVieW);
+elementsSet.case.addEventListener('mouseout', upButVieW);
 elementsSet.upButton.addEventListener('click', upButVieW);
