ce949da1998c134f5fd72676f2dcf122862517e7
Ursx2204 <127949449+Yurii2276@users.noreply.github.com>
2023-09-01 19:40:04 +0300

    зробив скрол і карент і адаптів

--- src/js/categoriesList.js
+++ src/js/categoriesList.js
@@ -2,31 +2,68 @@ import { fetchCategoriesList } from '../services/books-api';
 
 const refs = {
   container: document.querySelector(`.all-categories-box`),
+  title: document.querySelector(`.all-categoris-title`),
+  element: document.querySelector(`.all-categoris-element`),
 };
 
-console.log(`privet`);
+let elementHaveCurrent;
 
 fetchCategoriesList()
   .then(response => {
     const categories = response.data;
     const markup = createMarkup(categories);
-    refs.container.insertAdjacentHTML(
-      `afterbegin`,
-      `${markup}`
-    );
+    refs.container.insertAdjacentHTML(`beforeend`, `${markup}`);
   })
   .catch(error => {
     console.error('Error:', error);
   });
 
+refs.title.classList.add('current');
+
+refs.container.addEventListener(`click`, showCategoryBook);
+
 function createMarkup(array) {
   return array
     .map(
       categori =>
-        `<ul>
-            <li class="all-categoris-list">${categori.list_name}</li>
-        </ul> 
-        `
+        `<li class="all-categoris-list">
+      <a class="all-categoris-element" href="" data-name="${categori.list_name}"
+        >${categori.list_name}</a
+      >
+    </li>
+      `
     )
     .join(``);
 }
+
+function showCategoryBook() {
+  event.preventDefault(); 
+  
+  const clickedElement = event.target;
+
+  const categoryName = clickedElement.dataset.name;
+  console.log(`Clicked on category: ${categoryName}`);
+  changeCurrent(clickedElement);
+
+
+
+
+  
+}
+  
+function changeCurrent(curentClic) {
+
+  const allCategoryListItems = document.querySelectorAll('.all-categoris-list a');
+  allCategoryListItems.forEach(item => {
+    if (item.classList.contains('current')) {
+      elementHaveCurrent = item; 
+    }
+  });
+
+  if (curentClic !== elementHaveCurrent) {
+    curentClic.classList.add('current');
+    elementHaveCurrent.classList.remove('current');
+    } 
+}
+
+
