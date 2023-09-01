8c718994b400615a396aae50dae2fbb54b12f24e
OlenaOP <elena.peredriy@gmail.com>
2023-09-01 13:55:44 +0300

    add modal html-css

--- src/js/modalBook.js
+++ src/js/modalBook.js
@@ -1,1 +1,26 @@
+const BookContainer = document.querySelector('.container-books');
+BookContainer.addEventListener('click', openModal);
 
+const modalBook = document.querySelector('[data-modal="1"]');
+
+const closeModalBtn = document.querySelector('.modal-btn-close');
+closeModalBtn.addEventListener('click', closeModal);
+
+function openModal(event) {
+  if (event.target.parentNode.nodeName !== 'LI') {
+    return;
+  } else {
+    modalBook.classList.toggle('hidden');
+    modalBook.classList.toggle('active');
+    addModalBookMurkup();
+  }
+}
+
+function closeModal(event) {
+  modalBook.classList.toggle('hidden');
+  modalBook.classList.toggle('active');
+}
+
+function addModalBookMurkup() {
+  //   console.log('add');
+}
