1ee5fe15270f98469772d407acede95d114dc6e3
argojkee <maksymhaidabura@gmail.com>
2023-09-01 22:02:30 +0300

    Update bookMarkets.js

--- src/js/bookMarkets.js
+++ src/js/bookMarkets.js
@@ -2,6 +2,6 @@ import '../images/bookMarkets/bookMarkets-x1/amazon.webp';
 import '../images/bookMarkets/bookMarkets-x1/tradeBook2.webp';
 import '../images/bookMarkets/bookMarkets-x1/tradeBook3.webp';
 
-//  amazon = '/amazon.fc7bd580.webp';
-// book2 = '/tradeBook2.c2b1ff53.webp';
-//  book3 ='/tradeBook3.1410ecbf.webp';
+//  amazon src= '/amazon.fc7bd580.webp';
+// book2 src= '/tradeBook2.c2b1ff53.webp';
+//  book3 src='/tradeBook3.1410ecbf.webp';
