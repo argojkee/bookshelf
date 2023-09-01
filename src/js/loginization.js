ff7c7edaf494bb0896e1ebc0a2673fc1be2f5b04
OsMax <www.osmax@gmail.com>
2023-09-02 00:00:59 +0300

    for login

--- src/js/loginization.js
+++ src/js/loginization.js
@@ -1,1 +1,35 @@
+import { logUp, logIn } from './loginApi';
 
+const form = document.getElementById('formUp');
+const bUp = document.querySelector('.sUpBtb');
+const bIn = document.querySelector('.sInBtb');
+
+const nameLabel = document.getElementById('nameLabel');
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const pass = document.getElementById('pass');
+
+let logIs = true;
+
+bUp.addEventListener('click', () => {
+  nameLabel.hidden = false;
+  logIs = true;
+});
+bIn.addEventListener('click', () => {
+  nameLabel.hidden = true;
+  logIs = false;
+});
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  sing();
+  form.reset();
+});
+
+function sing() {
+  if (logIs) {
+    logUp(name.value, email.value, pass.value);
+  } else {
+    logIn(email.value, pass.value);
+  }
+}
