4e4018315effabd62d52bb10859d9f3818b50f8c
argojkee <maksymhaidabura@gmail.com>
2023-09-01 18:41:25 +0300

    try webp

--- src/js/fondsList.js
+++ src/js/fondsList.js
@@ -1,1 +1,12 @@
+// import dataFonds from '../data/fonds.json';
 
+// const listEl = document.querySelector('.fond-list');
+
+// function makeMarkup(arr) {
+//   return arr.map(
+//     fond =>
+//       `<li><a href='${fond.url}'><img src='${fond.img}' alt='${fond.title}'/> <h2>${fond.title}</h2></a></li>`
+//   );
+// }
+
+// listEl.innerHTML = makeMarkup(dataFonds);
