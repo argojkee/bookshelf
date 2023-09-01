61d691ca47a0f21b60a4ca074c6167866413efc7
Arrdroid <alexmirensko@gmail.com>
2023-09-01 21:05:24 +0300

    summary

--- src/js/header.js
+++ src/js/header.js
@@ -13,14 +13,24 @@ document.querySelector('.themetoggle').addEventListener('click', (event) => {
 function addDarkClassToHTML() {
   try {
     if (localStorage.getItem('theme') === 'dark') {
-      document.querySelector('html').classList.add('dark');
-      document.querySelector('.themetoggle').textContent = 'dark';
+      document.querySelector('body').classList.add('dark');
+      // document.querySelector('.themetoggle').textContent = 'dark';
     }
     else {
-      document.querySelector('html').classList.remove('dark');
-      document.querySelector('.themetoggle').textContent = 'Light';
+      document.querySelector('body').classList.remove('dark');
+      // document.querySelector('.themetoggle').textContent = 'Light';
     }
   } catch (err) { }
 }
 
 addDarkClassToHTML();
+
+  const burgerSwitch = document.querySelector('.burger-head');
+  const burgerCross = document.querySelector('.burger-cross');
+
+  document.querySelector('.js-cross-switch').addEventListener('click', (event) => {
+    event.preventDefault();
+    
+    burgerSwitch.classList.toggle("header-switch-hidden");
+    burgerCross.classList.toggle("header-switch-hidden");
+  });
