4aff7882e8c23e03300e57a992b2a0469347dcdd
Arrdroid <alexmirensko@gmail.com>
2023-09-02 21:41:29 +0300

    summary

--- src/js/header.js
+++ src/js/header.js
@@ -1,77 +1,79 @@
 import { getName } from './loginApi.js';
 
-  // Dark theme for local storage
-document.querySelector('.themetoggle').addEventListener('click', (event) => {
+// Dark theme for local storage
+document.querySelector('.themetoggle').addEventListener('click', event => {
   event.preventDefault();
   if (localStorage.getItem('theme') === 'dark') {
     localStorage.removeItem('theme');
+  } else {
+    localStorage.setItem('theme', 'dark');
   }
-  else {
-    localStorage.setItem('theme', 'dark')
-  }
-  switchToDarkTheme()
+  switchToDarkTheme();
 });
 
 function switchToDarkTheme() {
   try {
     if (localStorage.getItem('theme') === 'dark') {
       document.querySelector('body').classList.add('dark');
-    }
-    else {
+    } else {
       document.querySelector('body').classList.remove('dark');
     }
-  } catch (err) { }
+  } catch (err) {}
 }
 
 switchToDarkTheme();
 
-  // Burger button
-  const burgerSwitch = document.querySelector('.burger-head');
-  const burgerCross = document.querySelector('.burger-cross');
+// Burger button
+const burgerSwitch = document.querySelector('.burger-head');
+const burgerCross = document.querySelector('.burger-cross');
 
-  document.querySelector('.js-cross-switch').addEventListener('click', (event) => {
-    event.preventDefault();
-    
-    burgerSwitch.classList.toggle("header-switch-hidden");
-    burgerCross.classList.toggle("header-switch-hidden");
-  });
+document.querySelector('.js-cross-switch').addEventListener('click', event => {
+  event.preventDefault();
 
-  // Highlighting selected page in header with yellow color
+  burgerSwitch.classList.toggle('header-switch-hidden');
+  burgerCross.classList.toggle('header-switch-hidden');
+});
 
-    const shoppListBtn = document.querySelector(".js-shop-btn");
-    const headHomeBtn = document.querySelector(".header-home");
+// Highlighting selected page in header with yellow color
 
-    shoppListBtn.addEventListener('click', (event) => {
-      event.preventDefault();
+const shoppListBtn = document.querySelector('.js-shop-btn');
+const headHomeBtn = document.querySelector('.header-home');
 
-      shoppListBtn.classList.add("header-selected-page");
-      headHomeBtn.classList.remove("header-selected-page");
-    });
+if (document.location.href.includes('shopping')) {
+  shoppListBtn.classList.add('header-selected-page');
+  headHomeBtn.classList.remove('header-selected-page');
+} else {
+  shoppListBtn.classList.remove('header-selected-page');
+  headHomeBtn.classList.add('header-selected-page');
+}
 
-    headHomeBtn.addEventListener('click', (event) => {
-      event.preventDefault();
 
-      shoppListBtn.classList.remove("header-selected-page");
-      headHomeBtn.classList.add("header-selected-page");
-    })
-      
-    // Hiding and showing Signing up BTN and logging out BTN
+// Hiding and showing Signing up BTN and logging out BTN
 
-  function headBtnAuthorization() {
-    if (localStorage.getItem('bookshelId') && window.screen.availWidth >= 768) {
-      document.getElementById("head-logged-steven").style.display = "flex";
-      document.getElementById("head-signup-btn").style.display = "none";
-    }
-    else if(!localStorage.getItem('bookshelId') && window.screen.availWidth >= 768) {
-      document.getElementById("head-logged-steven").style.display = "none";
-      document.getElementById("head-signup-btn").style.display = "flex";
+export function headBtnAuthorization() {
+  if (window.screen.availWidth < 768) {
+    document.getElementById('head-logged-steven').style.display = 'none';
+    document.getElementById('head-signup-btn').style.display = 'none';
+  } else if (localStorage.getItem('bookshelId')) {
+    document.getElementById('head-logged-steven').style.display = 'flex';
+    document.getElementById('head-signup-btn').style.display = 'none';
+  } else if (!localStorage.getItem('bookshelId')) {
+    document.getElementById('head-logged-steven').style.display = 'none';
+    document.getElementById('head-signup-btn').style.display = 'flex';
+  }
 
-      // shoppListBtn.style.display = "none"
-      // headHomeBtn.style.display = "none"
+  hideHomeAndShop();
+}
 
-    
+  // Showing an hiding home and shopping
+  function hideHomeAndShop() {
+  if (!localStorage.getItem('bookshelId')) {
+    shoppListBtn.style.display = "none";
+    headHomeBtn.style.display = "none";
     }
   }
 
 
   headBtnAuthorization();
+
+window.addEventListener('resize', headBtnAuthorization);
