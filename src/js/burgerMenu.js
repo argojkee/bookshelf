f303dd876892936c6d6d4f739f1ac0c2fd188b70
argojkee <maksymhaidabura@gmail.com>
2023-09-03 22:04:18 +0300

    Update burgerMenu.js

--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -2,7 +2,7 @@ const menuBurger = document.querySelector('.js-cross-switch');
 menuBurger.addEventListener('click', onMenuOpen);
 
 function onMenuOpen(event) {
-  event.preventDafault();
+  event.preventDefault();
   if (event.target.include()) {
     return;
   }
