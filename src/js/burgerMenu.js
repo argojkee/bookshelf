09f3d6b1d98ff2a4d04ececb7922237ca6e58952
ExLuZiVe53 <imstudent27@gmail.com>
2023-09-03 15:07:28 +0200

    add burger-js

--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -1,1 +1,15 @@
+const burgerOpenMenu = document.querySelector('.js-cross-switch');
+const openBackdrop = document.querySelector('.backdrop-burger');
 
+burgerOpenMenu.addEventListener('click', onOpenMenu);
+
+function onOpenMenu(event) {
+  burgerOpenMenu.classList.add('is-active');
+  if (burgerOpenMenu.include('is-active')) {
+    openBackdrop.style.display = 'block';
+    console.log(openBackdrop.style.display);
+  } else {
+    burgerOpenMenu.classList.remove('is-active');
+    openBackdrop.style.display = 'none';
+  }
+}
