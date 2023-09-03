757ad4663f9d77fff44723884798bb8654dd3fe6
Igor Lunyakin <lunyaigor484@gmail.com>
2023-09-03 17:36:29 +0300

    add pagination

--- src/js/shoppingList.js
+++ src/js/shoppingList.js
@@ -2,6 +2,8 @@ import amazonImage from '../images/bookMarkets/bookMarkets-x1/amazon.webp';
 import appleBookImage from '../images/bookMarkets/bookMarkets-x1/tradeBook2.webp';
 import bookshopImage from '../images/bookMarkets/bookMarkets-x1/tradeBook3.webp';
 import { getBase, addBase } from './loginApi';
+import Pagination from 'tui-pagination';
+import 'tui-pagination/dist/tui-pagination.css';
 
 const lorem =
   'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae ipsa possimus debitis dignissimos ipsum consequuntur voluptatibus facere vero itaque magni, vel veniam delectus provident molestiae id libero totam excepturi perferendis!';
@@ -12,13 +14,44 @@ const refs = {
     '.shopping-list-is-empty-message'
   ),
   title: document.querySelector('.shopping-list-title'),
+  container: document.getElementById('pagination'),
 };
+
+const options = {
+  itemsPerPage: 3,
+  visiblePages: 3,
+  page: 1,
+  centerAlign: false,
+  firstItemClassName: 'tui-first-child',
+  lastItemClassName: 'tui-last-child',
+  template: {
+    page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+    currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+    moveButton:
+      '<a href="#" class="tui-page-btn tui-{{type}}">' +
+        '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</a>',
+    disabledMoveButton:
+      '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+        '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</span>',
+    moreButton:
+      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+        '<span class="tui-ico-ellip">...</span>' +
+      '</a>'
+  }
+};
+
+if (document.documentElement.clientWidth < 768) {
+  options.itemsPerPage = 4;
+  options.visiblePages = 2;
+}
 
 getBase()
   .then(data => {
     if (data.length !== 0) {
       refs.shoppingListIsEmptyMessage.style.display = 'none';
-      createMarkup(data);
+      paginatePages(data);
       refs.title.style.marginBottom = '40px';
     } else {
       refs.shoppingListIsEmptyMessage.style.display = 'flex';
@@ -115,6 +148,28 @@ function createMarkup(books) {
   refs.ul.innerHTML = markup;
 }
 
+function paginatePages(arr) {
+  const lengthOfArr = arr.length;
+  options.totalItems = lengthOfArr;
+
+  if(lengthOfArr < options.page * 3 - 2) {
+    options.page -= 1;
+  }
+  
+  const pagination = new Pagination(refs.container, options);
+
+  const pageToDraw = arr.slice((3 * options.page - 3), (3 * options.page));
+
+  createMarkup(pageToDraw);
+
+  pagination.on('afterMove', (event) => {
+    const currentPage = event.page;
+    options.page = currentPage;
+    const pageContent = arr.slice((3 * currentPage - 3), (3 * currentPage));
+    createMarkup(pageContent);
+  });
+}
+
 async function deleteItem(event) {
   if (
     event.target.classList.contains('delete-btn-icon-use') ||
@@ -139,8 +194,6 @@ async function deleteItem(event) {
       refs.title.style.marginBottom = '40px';
     }
 
-    createMarkup(filteredBooks);
+    paginatePages(filteredBooks);
   }
-
-  return;
 }
