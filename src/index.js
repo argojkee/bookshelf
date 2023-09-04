d403b89e18b684104f5d73949fd583918b543836
argojkee <maksymhaidabura@gmail.com>
2023-09-04 20:06:31 +0300

    first step

--- src/index.js
+++ src/index.js
@@ -6,5 +6,6 @@ import './js/slick-slider';
 import './js/content';
 import './js/modalBook';
 import './js/burgerMenu';
+import './js/addUserPhoto';
 import './js/loginization';
 import './js/scrollup';
---
+++ src/js/addUserPhoto.js
@@ -0,0 +1,5 @@
+const userPhotoHeaderSvg = document.querySelector('.js-user-icon-header');
+const userPhotoBurgerSvg = document.querySelector('.js-user-icon-burger');
+
+userPhotoHeaderSvg.innerHTML = `<use xlink:href="#icon-userphoto"></use>`;
+userPhotoBurgerSvg.innerHTML = `<use xlink:href="#icon-userphoto"></use>`;
