1197276cfe4035ce7ce902863aed21ded96c1c1d
argojkee <maksymhaidabura@gmail.com>
2023-09-06 23:02:35 +0300

    fix

--- src/js/footer.js
+++ src/js/footer.js
@@ -26,240 +26,244 @@ const informGit = document.querySelector('.footer-inform-git');
 
 const bodyElement = document.querySelector('body');
 
-
 const developerElement = document.querySelector('.developer');
 const disableBodyScroll = bodyScrollLock.disableBodyScroll;
 const enableBodyScroll = bodyScrollLock.enableBodyScroll;
 
-
-let classArr = ['user0', 'user1', 'user2', 'user3', 'user4', 'user5', 'user6', 'user7', 'user8', 'user9', 'user10'];
+let classArr = [
+  'user0',
+  'user1',
+  'user2',
+  'user3',
+  'user4',
+  'user5',
+  'user6',
+  'user7',
+  'user8',
+  'user9',
+  'user10',
+];
 let elementSet = [];
 let bufferArr = [];
 let id = [];
 
 const usersData = {
-    "_0": {name: "",
-    position:"Front-End",
-    section: "",
-    gitURL: "",},
-    "_1": {name: "Yurii Yakovchuk",
-    position:"Front-End",
-    section: "Category list",
-    gitURL: "",},
-    "_2": {name: "Dmytro Shevchenko",
-    position:"Front-End",
-    section: "ModalWindow",
-    gitURL: "",},
-    "_3": {name: "Maksym Osovik",
-    position:"Front-End",
-    section: "Authorization",
-    gitURL: "",},
-    "_4": {name: "Oleksandr Kalinovskyi",
-    position:"Scrum Master",
-    section: "Funds",
-    gitURL: "",},
-    "_5": {name: "Maksym Haidabura",
-    position:"Team Lead",
-    section: "All project",
-    gitURL: "",},
-    "_6": {name: "Olena Peredrii",
-    position:"Front-End",
-    section: "Modal window",
-    gitURL: "",},
-    "_7": {name: "Yurii Vovk",
-    position:"Front-End",
-    section: "Mobile menu",
-    gitURL: "",},
-    "_8": {name: "Oleksii Melnichenko",
-    position:"Front-End",
-    section: "Header",
-    gitURL: "",},
-    "_9": {name: "Ruslana Matviienko",
-    position:"Front-End",
-    section: "Main content",
-    gitURL: "",},
-    "_10": {name: "Igor Lunyakin",
-    position:"Front-End",
-    section: "Shopping list",
-    gitURL: "",},
-}
+  _0: { name: '', position: 'Front-End', section: '', gitURL: '' },
+  _1: {
+    name: 'Yurii Yakovchuk',
+    position: 'Front-End',
+    section: 'Category list',
+    gitURL: '',
+  },
+  _2: {
+    name: 'Dmytro Shevchenko',
+    position: 'Front-End',
+    section: 'ModalWindow',
+    gitURL: '',
+  },
+  _3: {
+    name: 'Maksym Osovik',
+    position: 'Front-End',
+    section: 'Authorization',
+    gitURL: '',
+  },
+  _4: {
+    name: 'Oleksandr Kalinovskyi',
+    position: 'Scrum Master',
+    section: 'Funds',
+    gitURL: '',
+  },
+  _5: {
+    name: 'Maksym Haidabura',
+    position: 'Team Lead',
+    section: 'All project',
+    gitURL: '',
+  },
+  _6: {
+    name: 'Olena Peredrii',
+    position: 'Front-End',
+    section: 'Modal window',
+    gitURL: '',
+  },
+  _7: {
+    name: 'Yurii Vovk',
+    position: 'Front-End',
+    section: 'Mobile menu',
+    gitURL: '',
+  },
+  _8: {
+    name: 'Oleksii Melnichenko',
+    position: 'Front-End',
+    section: 'Header',
+    gitURL: '',
+  },
+  _9: {
+    name: 'Ruslana Matviienko',
+    position: 'Front-End',
+    section: 'Main content',
+    gitURL: '',
+  },
+  _10: {
+    name: 'Igor Lunyakin',
+    position: 'Front-End',
+    section: 'Shopping list',
+    gitURL: '',
+  },
+};
 
 const addMouseEvent = () => {
+  const fifthElement = document.querySelector('[data-number = "5"]');
+  fifthElement.removeEventListener('mouseover', active);
+  fifthElement.removeEventListener('mouseout', lockActive);
+  fifthElement.addEventListener('mouseover', active);
+  fifthElement.addEventListener('mouseout', lockActive);
+  fifthElement.style.borderColor = 'blue';
+};
 
-    const fifthElement = document.querySelector('[data-number = "5"]');
-    fifthElement.removeEventListener("mouseover", active);
-    fifthElement.removeEventListener("mouseout", lockActive);
-    fifthElement.addEventListener("mouseover", active);
-    fifthElement.addEventListener("mouseout", lockActive);
-    fifthElement.style.borderColor = "blue";
-}
+const userMainInform = data => {
+  // Put Name
+  informName.textContent = usersData[data].name;
+  // Put section
+  informSection.textContent = usersData[data].section;
+  // Put gitURL
+  informGit.textContent = usersData[data].gitURL;
+  // Put position
+  informPosition.textContent = usersData[data].position;
+};
 
-const userMainInform = (data) => {
-    
+const createElementbyId = data => {
+  // console.log(id);
+  if (id.length !== 0) {
+    id[0].dataset.number = 0;
 
-    // Put Name
-    informName.textContent = usersData[data].name;
-    // Put section
-    informSection.textContent = usersData[data].section;
-    // Put gitURL
-    informGit.textContent = usersData[data].gitURL;
-    // Put position
-    informPosition.textContent = usersData[data].position;
+    id.splice(0, 1);
+  }
 
-}
+  for (let i = 0; i < data.length; i += 1) {
+    let bindUserAndClass = document.querySelector(`#_${i}`);
 
-const createElementbyId = (data) => {
-    // console.log(id);
-    if(id.length !== 0) {
+    // console.log(bindUserAndClass);
+    bindUserAndClass.classList.remove(`${elementSet[i]}`);
+    bindUserAndClass.classList.add(`${data[i]}`);
 
-        id[0].dataset.number = 0;
+    if (bindUserAndClass.classList.contains('user5')) {
+      bindUserAndClass.dataset.number = 5;
+      id.splice(0, 0, bindUserAndClass);
 
-        id.splice(0,1);
+      let informRow = bindUserAndClass.getAttribute('id');
+      // output main information about user
+      userMainInform(informRow);
     }
 
-    for(let i=0; i < data.length; i += 1) {
-
-        let bindUserAndClass = document.querySelector(`#_${i}`);
-
-        // console.log(bindUserAndClass);
-        bindUserAndClass.classList.remove(`${elementSet[i]}`);
-        bindUserAndClass.classList.add(`${data[i]}`);
-
-        if(bindUserAndClass.classList.contains("user5")) {
-            
-            bindUserAndClass.dataset.number = 5;
-            id.splice(0, 0, bindUserAndClass);
-
-            let informRow = bindUserAndClass.getAttribute("id");
-            // output main information about user
-            userMainInform(informRow);
-        }
-
-        //clear previous class
-        elementSet.splice(i, 1, data[i]);
-    }
-    addMouseEvent();
+    //clear previous class
+    elementSet.splice(i, 1, data[i]);
+  }
+  addMouseEvent();
 };
 
 const left = () => {
-    bufferArr[0] = classArr[0];
-    // delete first element
-    classArr.splice(0,1);
-    // add first element to array end
-    classArr.splice(classArr.length,0, bufferArr[0]);
-    console.log(classArr);
-   
-    createElementbyId(classArr);
+  bufferArr[0] = classArr[0];
+  // delete first element
+  classArr.splice(0, 1);
+  // add first element to array end
+  classArr.splice(classArr.length, 0, bufferArr[0]);
+  console.log(classArr);
+
+  createElementbyId(classArr);
 };
 
 const right = () => {
-    bufferArr[1] = classArr[classArr.length - 1];
-    classArr.splice(classArr.length - 1, 1);
-    classArr.splice(0,0, bufferArr[1]);
-    // console.log(classArr);
-   
-    createElementbyId(classArr);
+  bufferArr[1] = classArr[classArr.length - 1];
+  classArr.splice(classArr.length - 1, 1);
+  classArr.splice(0, 0, bufferArr[1]);
+  // console.log(classArr);
+
+  createElementbyId(classArr);
 };
 
-const wheelEvent = (e) => {
+const wheelEvent = e => {
+  if (e.deltaY > 60) {
+    right();
+  }
 
-    if(e.deltaY > 60) {
-        right();
-    }
-
-    if(e.deltaY < -60) {
-        left();
-    }
+  if (e.deltaY < -60) {
+    left();
+  }
 };
 
 const active = () => {
-    
-    let scrollBody = window.innerWidth - document.body.offsetWidth + 'px';
+  let scrollBody = window.innerWidth - document.body.offsetWidth + 'px';
 
-    bodyElement.style.paddingRight = scrollBody;
-    
-    // developerElement.style.paddingRight = scrollBody;
+  bodyElement.style.paddingRight = scrollBody;
 
-    pointerBatton.classList.toggle('pointer-hidden');
-   
-    fifthElement.style.borderColor = "blue";
-  
-    informWindow.style.visibility = "visible";
+  // developerElement.style.paddingRight = scrollBody;
 
-    disableBodyScroll(fifthElement);
-    addEventListener("wheel", wheelEvent);
+  pointerBatton.classList.toggle('pointer-hidden');
 
+  fifthElement.style.borderColor = 'blue';
+
+  informWindow.style.visibility = 'visible';
+
+  disableBodyScroll(fifthElement);
+  addEventListener('wheel', wheelEvent);
 };
 
 const lockActive = () => {
-    
-    bodyElement.style.paddingRight = 0;
+  bodyElement.style.paddingRight = 0;
 
-    pointerBatton.classList.toggle('pointer-hidden');
-    informWindow.classList.toggle('inform-hidden');
+  pointerBatton.classList.toggle('pointer-hidden');
+  informWindow.classList.toggle('inform-hidden');
 
+  fifthElement.style.borderColor = 'grey';
 
-    fifthElement.style.borderColor = "grey";
-   
-    informWindow.style.visibility = "hidden";
+  informWindow.style.visibility = 'hidden';
 
-    enableBodyScroll(fifthElement);
-    removeEventListener("wheel", wheelEvent);
-
+  enableBodyScroll(fifthElement);
+  removeEventListener('wheel', wheelEvent);
 };
 
+const bodyEvent = e => {
+  if (!e.composedPath().includes(footerSectionElement)) {
+    footerCover.classList.remove('hidden');
+    bodyElement.removeEventListener('click', bodyEvent);
+    footerCover.style.zIndex = '8';
+    footerButton.style.display = 'block';
 
-const bodyEvent = (e) => {
-
-    if(!e.composedPath().includes(footerSectionElement)) {
-
-        footerCover.classList.remove('hidden');
-        bodyElement.removeEventListener('click', bodyEvent)
-        footerCover.style.zIndex = "8";
-        footerButton.style.display = 'block';
-
-        informWindow.style.visibility = 'hidden';
-        leftButton.removeEventListener('click', leftEvent);
-        rightButton.removeEventListener('click', rightEvent);
-    }
-        
+    informWindow.style.visibility = 'hidden';
+    leftButton.removeEventListener('click', leftEvent);
+    rightButton.removeEventListener('click', rightEvent);
+  }
 };
 
-const leftEvent = (e) => {
-    
-        left();
+const leftEvent = e => {
+  left();
 };
 
-const rightEvent = (e) => {
-    
-        right();
+const rightEvent = e => {
+  right();
 };
-
 
 const buttonEvent = () => {
+  if (document.documentElement.scrollWidth <= 1439) {
+    informWindow.style.visibility = 'visible';
+    fifthElement.removeEventListener('mouseover', active);
+    fifthElement.removeEventListener('mouseout', lockActive);
+    fifthElement.style.borderColor = 'grey';
+  }
 
-    if(document.documentElement.scrollWidth <= 1439 ) {
-        informWindow.style.visibility = 'visible';
-        fifthElement.removeEventListener("mouseover", active);
-        fifthElement.removeEventListener("mouseout", lockActive);
-        fifthElement.style.borderColor = "grey";
-    }
-    
+  document.addEventListener('click', bodyEvent);
 
-    document.addEventListener('click', bodyEvent);
+  leftButton.addEventListener('click', leftEvent);
+  rightButton.addEventListener('click', rightEvent);
 
-    leftButton.addEventListener('click', leftEvent);
-    rightButton.addEventListener('click', rightEvent);
+  footerCover.classList.add('hidden');
+  footerCover.style.zIndex = '0';
+  footerButton.style.display = 'none';
 
-    footerCover.classList.add('hidden');
-    footerCover.style.zIndex = "0";
-    footerButton.style.display = 'none';
-
-    if(document.documentElement.scrollWidth > 1439 ) {
-
-        addMouseEvent();
-    }
-}
+  if (document.documentElement.scrollWidth > 1439) {
+    addMouseEvent();
+  }
+};
 
 footerButton.addEventListener('click', buttonEvent);
 
