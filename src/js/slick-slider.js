c038a86ab16e32bcc3546810fbc5ef75622fc536
argojkee <maksymhaidabura@gmail.com>
2023-09-07 17:19:09 +0300

    Update slick-slider.js

--- src/js/slick-slider.js
+++ src/js/slick-slider.js
@@ -1,19 +1,25 @@
 import $ from 'jquery';
 import 'slick-carousel';
 
-$('.carousel').slick({
-  // dots: true, //pagination
-  slidesToShow: window.screen.availWidth < 768 ? 4 : 6,
-  vertical: true,
-  // autoplay: true,
-  autoplaySpeed: 1000,
-  prevArrow: false,
-  nextArrow:
-    '<button type="button" arial-label="show next fund" class="slick-next"><svg class="slick-icon" width="22" height="22"><use  xlink:href="#icon-arrow-up"></use></svg></button>',
-  draggable: true,
-  // easing: 'string' animation
-  verticalSwiping: true,
-  // slidesToScroll: 3,
-  // centerMode: true,
-  initialSlide: 0,
-});
+refreshSlider();
+
+function refreshSlider() {
+  $('.carousel').slick({
+    // dots: true, //pagination
+    slidesToShow: window.screen.availWidth < 768 ? 4 : 6,
+    vertical: true,
+    // autoplay: true,
+    autoplaySpeed: 1000,
+    prevArrow: false,
+    nextArrow:
+      '<button type="button" arial-label="show next fund" class="slick-next"><svg class="slick-icon" width="22" height="22"><use  xlink:href="#icon-arrow-up"></use></svg></button>',
+    draggable: true,
+    // easing: 'string' animation
+    verticalSwiping: true,
+    // slidesToScroll: 3,
+    // centerMode: true,
+    initialSlide: 0,
+  });
+}
+
+window.addEventListener('resize', refreshSlider);
